e8fff68f5f468ae9dfd74a824a2cb52a4a01a738
jefringio <jefrinkalpurackal@gmail.com>
2019-09-05 13:42:39 +0530

    updated basic game completed

--- src/Alienship.java
+++ src/Alienship.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.io.*;
@@ -5,75 +6,81 @@ import java.util.*;
 import java.util.regex.*;
 public class Alienship {
     int input;
-    int ship1=3;
-    int ship2=3;
-    int ship3=3;
+    int[] temparray = new int[3];
     boolean shipkilled=false;
     int pos;
-    ArrayList<Integer> shipposition = new ArrayList<Integer>();
     int guess =0;
+    int invalidguess =0;
+
+    ArrayList<Integer> shipposition = new ArrayList<Integer>();
+    ArrayList<Integer> ship1 = new ArrayList<Integer>();
+    ArrayList<Integer> ship2 = new ArrayList<Integer>();
+    ArrayList<Integer> ship3 = new ArrayList<Integer>();
+
+
     public void getinput (int value ){
         input=value;
     }
+
     public void checkhitmiss() {
 
         if (shipposition.contains(input)) {
 
             pos = shipposition.indexOf(input);
-                if(pos<3){
-                    ship1-=1;
+            if (pos < 3) {
+                if (ship1.contains(input)) {
+                    System.out.println("You already entered " + input + ".");
+                    invalidguess++;
                 }
-                if(pos>=3 && pos<6 ){
-                    ship2-=1;
+                else {
+                    ship1.add(input);
+                    System.out.println("hit");
+                    guess++;
                 }
-                if(pos>=6 && pos<9){
-                    ship3-=1;
+                if(ship1.size()==3 ) {
+                    System.out.println("!!!Ship-1 sunk!!!");
                 }
-
-            System.out.println("It is a HIT!!!");
-
-            guess++;
-
-            if(ship1==0){
-                System.out.println("Ship 1 is Blasted");
-                if (ship1<0){
-                    System.out.println("!!!!!!You entered the position twice!!!!!!");
-                }
-
             }
-             if(ship2==0){
-                 System.out.println("Ship 2 is Blasted");
-                 if (ship2<0){
-                     System.out.println("!!!!!!You entered the position twice!!!!!!");
-                 }
-
-            }
-            if(ship3==0){
-                System.out.println("Ship 3 is Blasted");
-                if (ship3<0){
-                    System.out.println("!!!!!!You entered the position twice!!!!!");
+            else if (pos >= 3 && pos < 6) {
+                if (ship2.contains(input)) {
+                    System.out.println("You already entered " + input + ".");
+                    invalidguess++;
                 }
-
+                else {
+                    ship2.add(input);
+                    System.out.println("hit");
+                    guess++;
+                }
+                if(ship2.size()==3 ) {
+                    System.out.println("!!!Ship-2 sunk!!!");
+                }
             }
-            if (ship1==0 && ship2==0 && ship3==0) {
+            else if (pos >= 6 && pos < 9) {
+                if (ship3.contains(input)) {
+                    System.out.println("You already entered " + input + ".");
+                    invalidguess++;
+                }
+                else {
+                    ship3.add(input);
+                    System.out.println("hit");
+                    guess++;
+                }
+                if(ship3.size()==3 ) {
+                    System.out.println("!!!Ship-3 sunk!!!");
+                }
+            }
+            if((ship1.size()==3 ) && (ship2.size()==3) &&(ship3.size()==3)){
                 shipkilled= true;
-
                 System.out.println("You won the GAME!!!!!");
-                System.out.println("and the total guess is :" + guess);
+                System.out.println("and the total valid guesses are : " + (guess));
+                System.out.println("and the total guesses with multiple entry are : " + (guess+invalidguess));
             }
         }
         else{
-            System.out.println("It is a MISS!!!");
+            System.out.println("miss");
             guess++;
         }
-
-
     }
-
-
-
-
-
 
 //    public void convertRegexToNumber (String value ){
 //        boolean b = Pattern.matches("[a-h|A-H][1-8]" , value);
@@ -89,67 +96,107 @@ public class Alienship {
         int randomship = 0;
         int randomdir = 0;
 
-        for(int i = 0; i < 3; i++) {
-            int flag=1;
+
+        for(int i = 0; i < 4; i++) {
+
+            boolean flag =true;
             randomship = (int) (Math.random() * 64) + 0;
             randomdir = (int) (Math.random() * 2) + 0;
-            while(flag==1){
 
-                    if (shipposition.contains(randomship)) {
+            while (flag ) {
+                if ((temparray[0] != 0) && (temparray[1] != 0) && (temparray[1] != 0)) {
+                    if (shipposition.contains(temparray[0]) == false && shipposition.contains(temparray[1]) == false
+                            && shipposition.contains(temparray[2]) == false) {
+
+                        shipposition.add(temparray[0]);
+                        shipposition.add(temparray[1]);
+                        shipposition.add(temparray[2]);
+                    } else {
                         randomship = (int) (Math.random() * 64) + 0;
+                        if (shipposition.contains(randomship)) {
+                            flag = true;
+
+                        } else
+                            flag = false;
+
                     }
-
-                else {
-                    flag=0;
                 }
+                else
+                    flag=false;
             }
-            System.out.println("direction: " + randomdir);
-            System.out.println(randomship);
 
-            if (randomdir == 0) {
-                if ((randomship % 8) == 0) {
-                    shipposition.add(randomship);
-                    shipposition.add(randomship + 1);
-                    shipposition.add(randomship + 2);
-//                    System.out.println(randomship + " " + (randomship + 1) + " " + (randomship + 2));
-                } else if (((randomship + 1) % 8) == 0) {
-                    shipposition.add(randomship - 2);
-                    shipposition.add(randomship - 1);
-                    shipposition.add(randomship);
-//                    System.out.println((randomship - 2) + " " + (randomship - 1) + " " + (randomship ));
+
+//            while(flag==1){
+//
+//                if (shipposition.contains(randomship)) {
+//                    randomship = (int) (Math.random() * 64) + 0;
+//                }
+//
+//                else {
+//                    flag=0;
+//                }
+//            }
+                System.out.println("direction: " + randomdir);
+                System.out.println(randomship);
+
+                // heck condition and insert random values to temarray[]..
+
+                if (randomdir == 0) {
+                    if ((randomship % 8) == 0) {
+                        temparray[0] = randomship;
+                        temparray[1] = randomship + 1;
+                        temparray[2] = randomship + 1;
+
+                    } else if (((randomship + 1) % 8) == 0) {
+
+                        temparray[0] = randomship - 2;
+                        temparray[1] = randomship - 1;
+                        temparray[2] = randomship;
+
+                    } else {
+                        temparray[0] = randomship - 1;
+                        temparray[1] = randomship;
+                        temparray[2] = randomship + 1;
+
+                    }
                 } else {
+                    if (randomship < 8) {
+                        temparray[0] = randomship;
+                        temparray[1] = randomship + 8;
+                        temparray[2] = randomship + 16;
 
-                    shipposition.add(randomship - 1);
-                    shipposition.add(randomship);
-                    shipposition.add(randomship + 1);
-//                    System.out.println((randomship - 1) + " " + (randomship ) + " " + (randomship + 2));
+                    } else if (randomship > 55) {
+                        temparray[0] = randomship - 16;
+                        temparray[1] = randomship - 8;
+                        temparray[2] = randomship;
 
-                }
-            }
-            else{
-                if (randomship < 8){
-                    shipposition.add(randomship );
-                    shipposition.add(randomship + 8);
-                    shipposition.add(randomship + 16);
-                    System.out.println(randomship + " " + (randomship + 8) + " " + (randomship + 16));
-                }
-                else if (randomship > 55){
-                    shipposition.add(randomship );
-                    shipposition.add(randomship - 8);
-                    shipposition.add(randomship - 16);
-//                    System.out.println((randomship-16) + " " + (randomship -8) + " " + (randomship ));
-                }
-                else{
-                    shipposition.add(randomship - 8);
-                    shipposition.add(randomship );
-                    shipposition.add(randomship + 8);
-//                    System.out.println((randomship-8) + " " + (randomship ) + " " + (randomship + 8));
+                    } else {
+                        temparray[0] = randomship - 8;
+                        temparray[1] = randomship;
+                        temparray[2] = randomship + 8;
 
-                }
-            }
 
-            // Output is different everytime this code is executed
+                    }
+                }
+
+            // Insert the the random values into array list
+
+//            if ( shipposition.contains(temparray[0])==false && shipposition.contains(temparray[1])==false
+//                    && shipposition.contains(temparray[2])==false ) {
+//
+//                shipposition.add(temparray[0]);
+//                shipposition.add(temparray[1]);
+//                shipposition.add(temparray[2] );
+//
+//            }
+//            else{
+//
+//            }
+
+
         }
+
+
         for (int i = 0; i < shipposition.size(); i++) {
             System.out.println(shipposition.get(i));
         }
--- src/Main.java
+++ src/Main.java
@@ -13,26 +13,32 @@ public class Main {
 //        string layout;
 //    }
     public static void main(String[] args) {
-        int userinput;
-        int xLength = 8;
-        int yLength = 8;
-        System.out.println("Hello and welcome to Battleship!");
-        System.out.println("There are 3 hidden ships.");
-        System.out.println("Please enter x and y coordinates (max coordinate of 0-7 ) you would " +
-                "like to hit!\n");
-        Alienship ship = new Alienship();
-        ship.shipSetPosition();
+            int userinput;
+            int xLength = 8;
+            int yLength = 8;
+            System.out.println("Hello and welcome to Battleship!");
+            System.out.println("There are 3 hidden ships.");
+            System.out.println("Please enter x and y coordinates (max coordinate of 0-7 ) you would " +
+                    "like to hit!\n");
 
-        while(ship.shipkilled !=true) {
-            Scanner coordinate = new Scanner(System.in);
-            System.out.println("Enter the point to hit");
-            userinput = coordinate.nextInt();
+        // object of ship is created
+            Alienship ship = new Alienship();
 
-            System.out.println(userinput);
-            ship.getinput(userinput);
-            ship.checkhitmiss();
+        // three ships are created
+            ship.shipSetPosition();
+
+            while(ship.shipkilled !=true) {
+
+                // Enter the guess
+                    Scanner coordinate = new Scanner(System.in);
+                    System.out.println("Enter the point to hit");
+                    userinput = coordinate.nextInt();
+                    ship.getinput(userinput);
+
+                // check whether HIT or MISS
+                    ship.checkhitmiss();
+            }
         }
-    }
 
 
 }
